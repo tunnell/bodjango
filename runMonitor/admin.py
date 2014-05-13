e14a231d3061c49710c1597d5b4e83c8b1a9d6db
Daniel Coderre <dan@xedaq00.(none)>
2014-05-13 20:28:30 +0200

    Initial commit

---
+++ runMonitor/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from runMonitor.models import runInfo
+# Register your models here.
+
+admin.site.register(runInfo)
---
+++ runMonitor/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from runMonitor import views
+
+urlpatterns = patterns('',
+#   url(r'^$', TemplateView.as_view(template_name='runMonitor/index.html')),
+   url(r'^$', views.index, name='index'),
+   url(r'^uptimedata', views.uptimedata, name='uptimedata'),
+#    url(r'^$', TemplateView.as_view(template_name='runMonitor/index.html')),
+
+)
+
+urlpatterns += staticfiles_urlpatterns()
